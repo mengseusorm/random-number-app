2779f880a78919305454658d2d7f4d455477ff52
Sorm Mengseu <sormmengseu96@gmail.com>
2025-03-06 18:26:21 +0700

    fix package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart'; 
-import 'package:getwidget/getwidget.dart';
 import 'package:random_number_app/views/page/randomNumberPage.dart';
 import 'package:get/get.dart'; 
 
--- lib/views/page/randomNumberPage.dart
+++ lib/views/page/randomNumberPage.dart
@@ -1,5 +1,4 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart'; 
 import 'package:flutter/material.dart'; 
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
